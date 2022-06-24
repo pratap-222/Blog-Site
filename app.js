e21a886309bef35ba306a0048fb7dd1f1fbc8fcb
{Your-name} <{your-email-id}>
2022-06-25 01:36:24 +0530

    Added procfile and updated app.js, package.json and gitignore file

--- app.js
+++ app.js
@@ -149,9 +149,11 @@ app.post("/edit/:postId", function(req, res) {
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started Successfully...");
 });
